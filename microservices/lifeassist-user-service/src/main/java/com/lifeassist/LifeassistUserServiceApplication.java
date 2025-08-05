e603e0efe3d1fa6fe836f71216fe9e606b942914
Hasnat alam <hasnatalamofficial@gmail.com>
2025-08-05 16:16:24 +0530

    first commit

---
+++ microservices/lifeassist-user-service/src/main/java/com/lifeassist/LifeassistUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lifeassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeassistUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeassistUserServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/lifeassist-user-service/src/test/java/com/lifeassist_/LifeassistUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeassist_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeassistUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
